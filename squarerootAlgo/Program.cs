fd88bace9bc3d8663bd06ed6e93d59b14e05d010
Hillbill1987 <49688915+Hillbill1987@users.noreply.github.com>
2019-07-11 15:59:16 +0100

    Add files via upload

---
+++ squarerootAlgo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace squarerootAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            numbers num = new numbers();
+            num.SquareRoot(5625);
+            num.ContinuesSquareRoot(625);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ squarerootAlgo/numbers.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace squarerootAlgo
+{
+
+    public class numbers
+    {
+
+        public void SquareRoot(int number)
+        {
+
+            if ((number % 1) == 0)
+            {
+                Double temp = (Math.Sqrt(number));
+
+
+                if ((temp % 1) == 0)
+                {
+                    Console.WriteLine("the square root of the number " + number + " is " + temp);
+                }
+                else
+                {
+                    Console.WriteLine("not a whole number");
+
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("banana");
+            }
+        }
+
+
+        public void ContinuesSquareRoot(int number)
+        {
+            Double temp = number;
+            if ((temp % 1) == 0)
+
+            {
+                do
+                {
+                    if ((temp % 1) == 0)
+                    {
+                        temp = Math.Sqrt(temp);
+
+                        if ((temp % 1) == 0)
+                        {
+                            Console.WriteLine(temp);
+                        }
+                        else
+                        {
+
+                            break;
+
+                        }
+
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+
+                }
+                while()
+                
+                else
+                    {
+
+                        Console.WriteLine("not a whole number");
+                    }
+                
+                
+               
+            }
+        }
+    }
+}
+        
+    
+
+
+
+
+
+
+
+
+
+           
+
+
+
+
+        
+    
+
+        
+
+      
+    
+
